5284464069755fd21f5072e6b3875920b1213e91
Matthieu Pinte <matthieu@nectify.com>
2014-11-28 16:42:16 +0100

    Start to receive call

--- proxy/app.js
+++ proxy/app.js
@@ -1,8 +1,21 @@
-var http   = require('http'),
-    twilio = require('twilio');
+var accountSid = 'AC0f402cf3e41f80af00c325ec6d496216';
+var authToken  = 'd3642044e051e48e6f81f4c4889388bd';
+var twilio     = require('twilio'),
+    client     = twilio(accountSid, authToken),
+    express    = require('express');
 
-http.createServer(function(req, res) {
+// Create express app with middleware to parse POST body
+var app = express();
 
-  // Loop, code goes here...
+// Create a route to respond to a call
+app.post('/incomingCall', function(req, res) {
+    //Validate that this request really came from Twilio...
+    var twiml = new twilio.TwimlResponse();
+    twiml.say('Hello from node.js!');
 
-}).listen(1337);
+    // Render the TwiML response as XML
+    res.type('text/xml');
+    res.send(twiml.toString());
+});
+
+app.listen(process.env.PORT || 3000);
